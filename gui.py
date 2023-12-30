eef56904733448047def5906ba0081243c8c2ad7
KatiaOuadahi <katiaouadahi16@gmail.com>
2023-12-30 17:22:17 +0100

    frame added

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 def hide_elements():
-    # Hide labels, entries, and button
+    #Hide labels, entries, and button
     sepal_length_input.grid_forget()
     sepal_length_label.grid_forget()
     sepal_width_input.grid_forget()
@@ -11,28 +11,40 @@ def hide_elements():
     petal_width_input.grid_forget()
     petal_width_label.grid_forget()
     classity_button.grid_forget()
+    
+
+
 
 root=Tk()
-root.geometry("600x300")
+root.geometry("600x350")
 root['background']='MediumPurple'
 root.title("❀ Iris Classification ❀")
 
 #Widgets
 
+#Frame
+Frame=LabelFrame(root,text="",bg="MediumPurple",padx=5,pady=10)
+Frame.pack(padx=20,pady=20)
+
+
+
 #Labels
-sepal_length_label = Label(root, text="Sepal Length",width=20 , font=('Helvetica', 16),bg='MediumPurple')
-sepal_width_label = Label(root, text="Sepal Width",width=20 , font=('Helvetica', 16),bg='MediumPurple')
-petal_length_label = Label(root, text="Petal Length",width=20 , font=('Helvetica', 16),bg='MediumPurple')
-petal_width_label = Label(root, text="Petal Width",width=20 , font=('Helvetica', 16),bg='MediumPurple')
+sepal_length_label = Label(Frame, text="Sepal Length",width=20 , font=('Helvetica', 16),bg='MediumPurple')
+sepal_length_label = Label(Frame, text="Sepal Length",width=20 , font=('Helvetica', 16),bg='MediumPurple')
+sepal_width_label = Label(Frame, text="Sepal Width",width=20 , font=('Helvetica', 16),bg='MediumPurple')
+petal_length_label = Label(Frame, text="Petal Length",width=20 , font=('Helvetica', 16),bg='MediumPurple')
+petal_width_label = Label(Frame, text="Petal Width",width=20 , font=('Helvetica', 16),bg='MediumPurple')
 
 #Inputs
-sepal_length_input = Entry(root, width=20 , font=('Helvetica', 16))
-sepal_width_input = Entry(root, width=20 , font=('Helvetica', 16))
-petal_length_input = Entry(root, width=20 , font=('Helvetica', 16))
-petal_width_input = Entry(root, width=20 , font=('Helvetica', 16))
+sepal_length_input = Entry(Frame, width=20 , font=('Helvetica', 16))
+#sepal_length_input.insert(0,"Entre sepal length :")
+
+sepal_width_input = Entry(Frame, width=20 , font=('Helvetica', 16))
+petal_length_input = Entry(Frame, width=20 , font=('Helvetica', 16))
+petal_width_input = Entry(Frame, width=20 , font=('Helvetica', 16))
 
 #Buttons
-classity_button = Button(root, text="Classify Now!",command=hide_elements,width=20 , font=('Helvetica', 16),bg='Purple', fg='white')
+classity_button = Button(Frame, text="Classify Now!",command=hide_elements,width=20 , font=('Helvetica', 16),bg='Purple', fg='white')
 
 
 # Grid Positioning
