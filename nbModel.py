dacc40277169902c97ed45e5b84af4a9b42849cc
DELL <saritta.loloka@gmail.com>
2023-12-31 12:59:50 +0100

    data splitted into training and testing set

--- nbModel.py
+++ nbModel.py
@@ -9,3 +9,8 @@ iris_data = pd.read_csv('iris_dataset.csv')
 # Prepare the Data for training
 X = iris_data[['sepal.length', 'sepal.width', 'petal.length', 'petal.width']]
 y = iris_data['variety']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+#train_test_split : shuffles the data before splitting, 
+#random_state parameter : ensures reproducibility in the shuffling process. 
