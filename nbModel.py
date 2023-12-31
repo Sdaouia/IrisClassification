19234007f4621daa7f8894aae61c668efed78b76
DELL <saritta.loloka@gmail.com>
2023-12-31 12:38:33 +0100

    iris dataset loaded

--- nbModel.py
+++ nbModel.py
@@ -2,3 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+
+# Load the iris dataset
+iris_data = pd.read_csv('iris_dataset.csv') 
