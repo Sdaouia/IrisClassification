1d59f819b55dff596e1ac5e45dc52a0cec85d284
DELL <saritta.loloka@gmail.com>
2023-12-31 23:01:35 +0100

    Model Evaluated

--- nbModel.py
+++ nbModel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score , classification_report , confusion_matrix
 
 # Load the iris dataset
 iris_data = pd.read_csv('iris_dataset.csv') 
@@ -23,3 +23,21 @@ nb_model.fit(X_train, y_train)
 
 # Make predictions
 y_pred = nb_model.predict(X_test)
+
+
+
+#Evaluate the performance of the model
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"The accuracy of the Naive Baye model is {accuracy:.2f}\nThis means that it can correctly predict the type of the Iris flower {round(accuracy * 100)}% of the time.\n")
+
+#calculate classification report
+print('Classification Report:')
+print(classification_report(y_test, y_pred))
+
+
+# Calculate confusion matrix
+conf_matrix = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(conf_matrix)
