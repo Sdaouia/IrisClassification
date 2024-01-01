bcb95dc92c3cf021abad8939b8da91e8a0e0bc13
DELL <saritta.loloka@gmail.com>
2024-01-01 15:01:17 +0100

    nbModel and nbIrisClassifier functions added

--- naiveBayes.py
+++ naiveBayes.py
@@ -2,8 +2,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import joblib
 
-def naiveBayesModel(file_path):
+
+#The "nbModel" function trains a Gaussian NB model on a dataset uploaded by the user, then returns the evaluation results of the NB model's performance
+
+def nbModel(file_path):
     # Load the iris dataset from the provided file
     iris_data = pd.read_csv(file_path) 
 
@@ -13,9 +17,6 @@ def naiveBayesModel(file_path):
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-    #train_test_split : shuffles the data before splitting, 
-    #random_state parameter : ensures reproducibility in the shuffling process. 
-
 
     # Initialize Gaussian Naive Bayes model
     nb_model = GaussianNB()
@@ -26,10 +27,7 @@ def naiveBayesModel(file_path):
     # Make predictions
     y_pred = nb_model.predict(X_test)
 
-
-
     # Evaluate the performance of the model
-
     # Calculate accuracy
     accuracy = accuracy_score(y_test, y_pred)
 
@@ -39,6 +37,9 @@ def naiveBayesModel(file_path):
     # Calculate confusion matrix
     conf_matrix = confusion_matrix(y_test, y_pred)
 
+    # Save the trained model to a file
+    joblib.dump(nb_model, 'naive_bayes_model.joblib')
+
     # Return the results as a dictionary
     results = {
         'accuracy': accuracy,
@@ -47,3 +48,37 @@ def naiveBayesModel(file_path):
     }
 
     return results
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+#The "nbIrisClassifier" function uses the trained Gaussian Nb model to classify an instance of iris data entered by the user
+
+def nbIrisClassifier(sepal_length, sepal_width, petal_length, petal_width):
+    try:
+        # Load the trained Naive Bayes model
+        nb_model = joblib.load('naive_bayes_model.joblib')
+    except FileNotFoundError:
+        print("Model file not found. Make sure to train the model first.")
+        return None
+
+
+    # Prepare the input data for prediction with feature names
+    new_instance = pd.DataFrame({
+        'sepal.length': [sepal_length],
+        'sepal.width': [sepal_width],
+        'petal.length': [petal_length],
+        'petal.width': [petal_width]
+    }, columns=['sepal.length', 'sepal.width', 'petal.length', 'petal.width'])
+
+
+    # Use the trained Naive Bayes model to make predictions
+    prediction = nb_model.predict(new_instance)
+
+    # Return the result of the classification
+    return prediction[0]
+
